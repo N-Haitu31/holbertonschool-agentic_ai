be8065f2979d5d9b0c22cdb489bf9e106ed991b1
N-Haitu31 <nguyenhaitu31@gmail.com>
2026-08-04 18:57:26 +0200

    feat(task-4): add features data

---
+++ front_end-frameworks/react/src/data/features.js
@@ -0,0 +1,10 @@
+import { Bot, Workflow, BrainCircuit, Database,  Wrench, ShieldCheck } from 'lucide-react';
+
+export const features = [
+    { icon: Bot, title: "Autonomous agents", description: "Deploy self-sufficient AI agents that can work 24/7 without supervision." },
+    { icon: Workflow, title: "Multi-step planning", description: "Break down complex goals into actionable steps with intelligent planning." },
+    { icon: BrainCircuit, title: "Advanced reasoning", description: "Leverage state-of-the-art language models for intelligent decision-making." },
+    { icon: Database, title: "Memory & context", description: "Persistent memory allows agents to learn and improve over time" },
+    { icon: Wrench, title: "Tool integration", description: "Connect to thousands of APIs and services seamlessly." },
+    { icon: ShieldCheck, title: "Enterprise Security", description: "Bank-level encryption and compliance with SOC2, GDPR, and HIPAA." },
+]
