42ead015dad8ecb0170371aff6d72875d83ce29d
N-Haitu31 <nguyenhaitu31@gmail.com>
2026-08-04 19:41:20 +0200

    feat(task-4): fix and add features section

--- front_end-frameworks/react/src/components/FeatureCard.jsx
+++ front_end-frameworks/react/src/components/FeatureCard.jsx
@@ -1,10 +1,10 @@
 function FeatureCard ({ icon: Icon, title, description}) {
     return (
-        <article className="p-8 rounded-3xl border border-slate-800 bg-slate-950 shadow-xl shadow-slate-950/40">
-            <div className="bg-violet-500 p-3 rounded-lg text-white">
+        <article className="text-left p-8 rounded-3xl border border-slate-800 bg-slate-950 shadow-xl shadow-slate-950/40">
+            <div className="inline-flex bg-violet-500 p-3 rounded-lg text-white mb-4">
                 <Icon />
             </div>
-            <h3 className="text-xl font-bold text-white">{title}</h3>
+            <h3 className="text-xl font-bold text-white mb-3">{title}</h3>
             <p className="text-slate-300">{description}</p>
         </article>
     )
---
+++ front_end-frameworks/react/src/sections/Features.jsx
@@ -0,0 +1,24 @@
+import { Sparkle } from 'lucide-react';
+import { features } from '../data/features'
+import FeatureCard from '../components/FeatureCard'
+
+function Features() {
+    return (
+        <section id="features-section" className="py-24 bg-slate-950">
+            <div className="max-w-6xl mx-auto px-6 flex flex-col items-center text-left">
+                <span className="mb-8 px-4 py-2 text-xs text-violet-300 rounded-full border border-violet-500/20 bg-violet-500/10 inline-flex items-center gap-2
+                "><Sparkle size={14} /> Features <Sparkle size={14} /></span>
+                <h2 className="mb-8 text-4xl md:text-5xl font-black tracking-tight leading-none">
+                    <span className="block text-white">Everything You Need to Build</span>
+                    <span className="block text-violet-300">With powerful AI agents</span>
+                </h2>
+                <div className="grid md:grid-cols-3 gap-8">
+                    {features.map((feature) => (
+                        <FeatureCard key={feature.title} icon={feature.icon} title={feature.title} description={feature.description} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Features
