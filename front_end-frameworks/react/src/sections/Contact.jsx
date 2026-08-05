e2cc122c55a6c8df0b452927586514f1050cb96d
N-Haitu31 <nguyenhaitu31@gmail.com>
2026-08-05 11:35:20 +0200

    feat(task-6): add contact section header and ctas

---
+++ front_end-frameworks/react/src/sections/Contact.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { UserRound, Mail, MessageSquare, FolderKanban, Users, Sparkle, ArrowRight } from 'lucide-react';
+
+function Contact() {
+    const [formData, setFormData] = useState({ fullName: "", email: "", message: "" });
+    const [isSending, setIsSending] = (useState(false));
+    const [feedbackMessage, setFeedbackMessage] = (useState("Please fill all required fields."));
+
+return (
+    <section id="contact-section" className="bg-slate-950 py-24">
+        <div className="max-w-6xl mx-auto px-6 flex flex-col items-center text-center">
+            <span className="mb-8 px-4 py-2 text-xs text-violet-300 rounded-full border border-violet-500/20 bg-violet-500/10 inline-flex items-center gap-2
+            "><Sparkle size={14} /> Start your AI journey <Sparkle size={14} /></span>
+            <h2 className="mb-8 text-4xl md:text-5xl font-black tracking-tight leading-none">
+                <span className="block text-white">Ready to Explore</span>
+                <span className="block text-violet-300">Agentic AI?</span>
+            </h2>
+            <div className="flex gap-4">
+                <a href="#" className="text-white px-4 py-2 font-semibold rounded-md inline-flex items-center gap-2 bg-violet-500 hover:bg-violet-600 shadow-lg shadow-violet-500/40">
+                Enroll at Holberton School <ArrowRight />
+                </a>
+                <a href="#" className="text-white px-4 py-2 font-semibold rounded-md border border-slate-800 bg-slate-950 hover:bg-slate-900">
+                    Need more information? 
+                </a>
+            </div>
+        </div>
+    </section>
+)
